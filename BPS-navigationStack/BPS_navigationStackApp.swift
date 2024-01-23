d0eedd6fbf92d5bf257c2cb8089d8d75d6fbf940
Simon <kantachat.puagkajee@7peakssoftware.com>
2024-01-23 12:49:17 +0700

    Initial Commit

---
+++ BPS-navigationStack/BPS_navigationStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  BPS_navigationStackApp.swift
+//  BPS-navigationStack
+//
+//  Created by Simon Siwell on 23/1/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct BPS_navigationStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BPS-navigationStack/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BPS-navigationStack
+//
+//  Created by Simon Siwell on 23/1/2567 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
